e2ac5eb4db13d6304a1e847e073872022c88532c
+ <+>
2022-10-29 16:29:39 -0400

    commit tree 5b1bc6ca4ee6fc018763e41b27e729d69f9cac2f

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result 
+  if (someValue <= 400) {
+    result =  'This one is on me!'
+  }
+  else if (someValue > 400 && someValue <2000) {
+    result = 'That will be twenty bucks.'
+  }
+  else if (someValue > 2000 && someValue <2500)  {
+    result = 'I will gladly take your thirty bucks.'
+  }
+  else {
+    result = 'No can do.'
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity (city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      case "thanks for everything":
+      return "Bye."
+  }
 }
